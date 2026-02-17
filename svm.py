4dbf94a6ffbc00288427c963203d19642ff7e709
Carlos Eduardo de Lima <carloselima373@gmail.com>
2026-02-17 20:34:57 -0300

    Código finalizado e comentado.

--- svm.py
+++ svm.py
@@ -1,12 +1,14 @@
+#Aqui está todas as bibliotecas necessárias para rodar o código.
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 
+#Aqui está a biblioteca que será utilizada para visualizar o SVM.
 base_dados_iris = datasets.load_iris()
 
+#Aqui temos uma função simples que constrói o gráfico e os pontos distribuídos no plano.
 def plot_svc_decision_function(model, ax=None):
 
     if ax is None:
@@ -25,23 +27,31 @@ def plot_svc_decision_function(model, ax=None):
     ax.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1], s=100, linewidth=1, facecolors='none', edgecolors='k')
 
 
+#Aqui nós estamos separando as variáveis que serão usadas para fazer a previsão.
 X = base_dados_iris.data[:, :2]
+
+#Aqui nós temos a variável alvo (Target), isto é, a variável que o modelo deve prever.
 Y = base_dados_iris.target
 
+#Aqui estamos definindo que os valores alvo não podem iguais a 2 (Iris-virginica). Isso é importante, pois, se não definirmos esse parâmetro, você estará tentando construir um plano 2D com dados em 3D.
 mask = (Y != 2)
 
 X = X[mask]
 Y = Y[mask]
 
+#Aqui estamos definindo os dados. Sobre esses dados, estamos separando 20% deles para testes. Além disso, estamos definindo a randomização dos dados em 42, de modo a evitar discrepâncias extremans conforme o código é rodado várias vezes.
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
+#Aqui estamos definindo o tipo de algoritmo usado e o tipo do Kernel, além do coeficiente C. Lembre-se, se o coeficiente for muito alto, caímos em Underfitting. Se for muito baixo, caímos em Overfitting.
 modelo_svm = SVC(kernel='linear', C=1.0)
+
+#Aqui estamos treinando o modelo com os dados de treino.
 modelo_svm.fit(X_train, Y_train)
 
+#Aqui estamos prevendo os valores de teste a partir do modelo que nós treinamos.
 Y_pred = modelo_svm.predict(X_test)
-print(classification_report(Y_test, Y_pred))
 
-
+#Por final, estamos construindo o gráfico que exibe a forma como os dados se comportam de acordo com os vetores. Note que os dados se separam por uma "linha invisível" muito nítida.
 plt.scatter(X[:, 0], X[:, 1], c=Y, s=50, cmap='autumn')
 plot_svc_decision_function(modelo_svm)
 plt.xlabel('Comprimento da Sépala')
